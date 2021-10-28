b2f9d4972935e7ff5104692260e61e9f8f23924b
wilmouths <wilmouthsteven@gmail.com>
2021-10-28 17:31:57 +0200

    feat: adding plugin source code

---
+++ src/server/types.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface EzreportingPluginSetup {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface EzreportingPluginStart {}
